a084ca2c12532350955b57ecefb5154f7b5150f7
ChoeDaeBeom <chleoqja0125@nate.com>
2021-12-21 23:05:06 +0900

    add

---
+++ src/server.js
@@ -0,0 +1,53 @@
+import http from 'http'
+import WebSocket from 'ws'
+import express from 'express'
+
+const app = express() 
+
+app.set("view engine", "pug")
+app.set("views", __dirname + "/views" )
+app.use("/public", express.static(__dirname+"/public"))
+app.get("/", (_, res)=> res.render("home"))
+app.get("/*",(_, res) => res.redirect('/'))
+
+
+const handleListen = () => console.log(`Listening on http://localhost:${3000}`)
+
+const server = http.createServer(app)
+const wss = new WebSocket.Server({server})
+
+const sockets = []
+
+wss.on("connection", (socket)=>{
+  sockets.push(socket)
+  socket['nickname'] = 'anonymous'
+
+  socket.on('close', ()=>{
+    console.log('disconnected from Browser')  
+  })
+
+  socket.on("message", (msg)=>{
+    const message = JSON.parse(msg)
+    console.log('parsed message: ', message)
+  
+    switch(message.type){
+      case 'new_message' : {
+        sockets.forEach(aSocket => {
+          console.log('message.payload: ', message.payload)
+          aSocket.send(`${socket.nickname}: ${message.payload}`)
+        })
+      }
+      case 'nickname' : {
+        socket['nickname'] = message.payload
+      }
+        
+    }
+     
+  })
+})
+
+wss.on("close", ()=>{
+  socket.send("close")
+})
+
+server.listen(3000, handleListen)
